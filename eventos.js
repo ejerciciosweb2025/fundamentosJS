cb60009c4bc7cb708f4a265ffbaadeec255a8dd7
ejerciciosweb2025 <ejerciciosweb2025@yahoo.com>
2025-02-27 19:19:25 +0100

    modificando index

---
+++ eventos.js
@@ -0,0 +1,16 @@
+/** Los eventos los programas están mandando sucesos 
+ * o mensajes constantemente. Estos sucesos pueden 
+ * ser provocados por el sistema, el usuario y otro 
+ * evento*/
+/**
+ * Cuando se provoca una acción que nuestro sistema
+ * puede interceptar e interpretar desembocará 
+ * la acción definida correspondiente para que se resuelva
+ * Un ejemplo, definimos la acción de pinchar, clickar 
+ * con el ratón. Cuando nosotros pinchemos ocurrirá lo que 
+ * hemos definido dentro de esa acción.
+ * 
+ * Los eventos serian como un conjunto de instrucciones
+ * que están esperando a ejecutarse cuando reciba el mensaje
+ * correspondiente a esa acción definida 
+ */
