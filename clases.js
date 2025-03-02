5cf177770dc01d1fdf73593fa561f6aca189a432
ejerciciosweb2025 <ejerciciosweb2025@yahoo.com>
2025-03-02 20:02:24 +0100

    JSON - Clases

--- clases.js
+++ clases.js
@@ -7,3 +7,13 @@
 //las propiedades son caracteristicas
 //los métodos son acciones que el objeto puede hacer
 
+//Las clases son un molde, una plantilla, plano, receta ...
+// que creara una estructura para un objeto concreto 
+//Ej. Clase un perro 
+
+//un objeto es una instancia,ejemplar único de esa clase
+//Ej. Objeto ¿que perro exactamente?
+//es de una raza concreta, estatura y nombre, es un 
+// perro único de cualquier otro
+
+
