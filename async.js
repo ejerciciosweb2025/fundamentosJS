b09999c665592523676cc98be3322e2194dd5205
ejerciciosweb2025 <ejerciciosweb2025@yahoo.com>
2025-03-02 23:38:40 +0100

    JSON - Clases

--- async.js
+++ async.js
@@ -11,7 +11,44 @@ async function asyncUsuarios(){
     console.log(data);
     //mi array de objetos
     console.log(data.data);
+    for( elemento of data.data ){
+        console.log(elemento);
+        let miusuario = document.querySelector("#usuario");
+
+        miusuario.innerHTML += `
+            <p>${elemento.first_name} -  ${elemento.last_name} </p>
+            <p> <img src="${elemento.avatar}" alt="usuario"></p>
+        `;
+    }
+
 
 }
 
 asyncUsuarios();
+
+console.log("Async Await con Try Catch");
+
+url = "https://reqres.in/api/users?page=2";
+
+async function asyncUsuarios2(){
+   try{
+    let response = await fetch(url);
+    let data     = await response.json();
+    console.log(data);
+    //mi array de objetos
+    console.log(data.data);
+    for( elemento of data.data ){
+        console.log(elemento);
+        let miusuario = document.querySelector("#usuario");
+
+        miusuario.innerHTML += `
+            <p>${elemento.first_name} -  ${elemento.last_name} </p>
+            <p> <img src="${elemento.avatar}" alt="usuario"></p>
+        `;
+    }
+   }catch(error){
+    console.log("Error al conseguir la información",error);
+   }
+}
+
+asyncUsuarios2();
