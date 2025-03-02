65975de2f3709a78ebc153fc216595a8ef2c90bf
ejerciciosweb2025 <ejerciciosweb2025@yahoo.com>
2025-03-02 21:54:00 +0100

    JSON - Clases

---
+++ matematicas.js
@@ -0,0 +1,42 @@
+console.log(Math.PI);
+//Elevar un número, potencia
+console.log(Math.pow(2,2));
+//Raíz cuadrada de un número
+console.log(Math.sqrt(4));
+
+//Valor absoluto
+console.log(Math.abs(5));
+console.log(Math.abs(-5));
+
+//Redondear de la alta a la baja
+console.log(Math.ceil(7.2));
+console.log(Math.round(7.2));
+console.log(Math.floor(7.2));
+
+//aleatorios del 0 al 1
+console.log(Math.random());
+
+//aleatorios del 0 al 10
+console.log(Math.random()*10);
+
+//aleatorios del 0 al 100
+console.log(Math.random()*100);
+
+
+//aleatorios del 0 al 1
+console.log(Math.ceil(Math.random()));
+
+//aleatorios del 0 al 10
+console.log(Math.ceil(Math.random()*10));
+
+//aleatorios del 0 al 100
+console.log(Math.ceil(Math.random()*100));
+
+console.log(Math.floor(Math.random()*100)+1);
+
+
+//valor máximo
+console.log(Math.max(1,5,50,3,27,17,2));
+
+//valor mínimo
+console.log(Math.min(1,5,50,3,27,17,2));
